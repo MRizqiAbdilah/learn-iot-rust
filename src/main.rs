357a26f688ebc485dd1842c61483db97137a1285
MRizqiAbdilah <muhammadrizqiabdilah10@gmail.com>
2026-06-04 10:23:46 +0700

    Day 3: Membuat tombol berperilaku sebagai toggle
    Materi:
    - GPIO Input, Masalah Floating Pin, dan Internal Pull-Up/Pull-Down
    - Panduan diagram.json ( pushbutton ) & Kode Rust ( gpio::{Input, Pull} )

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,11 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{delay::Delay, gpio::{Io, Level, Output}, prelude::*};
-
-fn nyalakan_alarm(delay: &Delay, pin_alarm: &mut Output<'_>) {
-    for _ in 0..5 {
-        pin_alarm.set_high();
-        delay.delay(200.millis());
-        pin_alarm.set_low();
-        delay.delay(200.millis());
-    }
-}
+use esp_hal::{
+    delay::Delay,
+    gpio::{Input, Io, Level, Output, Pull},
+    prelude::*,
+};
 
 #[entry]
 fn main() -> ! {
@@ -21,27 +16,43 @@ fn main() -> ! {
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    // led_biru = 5, led_merah = 6
-
-    // indikator pompa
-    let mut led_biru = Output::new(io.pins.gpio5, Level::Low);
-    // indikator alarm
     let mut led_merah = Output::new(io.pins.gpio6, Level::Low);
+
+    let mut led_aktif: bool = false;
+
+    let tombol = Input::new(io.pins.gpio2, Pull::Up);
 
     esp_println::logger::init_logger_from_env();
 
     loop {
-        log::info!("Fase Pengairan: Pompa menyala selama 4 detik...\r");
-        led_biru.set_high();
-        led_merah.set_low();
-        delay.delay(4000.millis());
+        // membuat seolah" tombol itu menjadi toggle
+        // tombol ditekan
+        if tombol.is_low(){
+            // Debouncing, jadi biar gak bisa spam
+            delay.delay(100.millis());
+            
+            // Cek kembali, apakah tombol ditekan? 
+            if tombol.is_low() {
+                // ini akan membuat led_aktif menjadi true
+                led_aktif = !led_aktif;
 
-        log::info!("Fase Peringatan Level Air: Alarm nyala sebanyak 5 kali...\r");
-        led_biru.set_low();
-        nyalakan_alarm(&delay, &mut led_merah);
+                // kalo led_aktif itu true
+                if led_aktif {
+                    // led_merah akan menyala dan akan menampilakn log Tombol: Aktif
+                    led_merah.set_high();
+                    log::info!("Tombol: Aktif\r")
+                } else {
+                    // led_merah akan mati dan akan menampilkan log Tombol: Tidak Aktif
+                    led_merah.set_low();
+                    log::info!("Tombol: Tidak Aktif\r")
+                }
 
-        log::info!("Fase Jeda: semua indikator mati dan akan kembali ke fase Pengairan dalam 2 detik... \r");
-        led_merah.set_low();
-        delay.delay(2000.millis());
+                // Jadi akan memberikan jeda, jika tombol dilepas
+                // dilakukan agar tidak tidak bisa melakukan tekan-lepas tombol berkali"
+                while tombol.is_low(){
+                    delay.delay(50.millis());
+                }
+            }
+        }
     }
 }
